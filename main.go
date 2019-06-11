94a2254e00665d75efa6e4fe5c4ce46e83e708de
Bjørn Tore Svinningen <postmannen@gmail.com>
2019-06-11 19:16:26 +0200

    upload working

---
+++ main.go
@@ -0,0 +1,139 @@
+/*
+	Test file upload on web page with MultiPart web page.
+	Will read the whole file into memory,
+	and write it all back to a temporary file.
+*/
+
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"image"
+	"image/jpeg"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/nfnt/resize"
+)
+
+func shrinkImage(inReader io.Reader, outWriter io.Writer, size uint) error {
+	decIm, _, err := image.Decode(inReader)
+	if err != nil {
+		return err
+	}
+
+	rezIm := resize.Resize(size, 0, decIm, resize.Lanczos3)
+
+	err = jpeg.Encode(outWriter, rezIm, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+const (
+	mainImageSize    = 700
+	thumbnailSize    = 200
+	serverListenPort = ":8080"
+)
+
+func (d *data) uploadImage(w http.ResponseWriter, r *http.Request) {
+	if err := d.templ.ExecuteTemplate(w, "upload", nil); err != nil {
+		log.Println("error: failed executing template for upload: ", err)
+	}
+
+	var err error
+
+	//Takes max size of form to parse.
+	err = r.ParseMultipartForm(32 << 20)
+	if err != nil {
+		log.Println("error: ParseMultipartForm: ", err)
+		//NB: NB: We have to return here if it fails, since the first show of the
+		// page no files are specified, and the page will panic on the code below
+		// if it continues and try to get the FormFile.
+		return
+	}
+
+	//Get a handler for the file found in the web form.
+	//Returns the first file for the provided key.
+	inFile, inFileHeader, err := r.FormFile("myFile")
+	if err != nil {
+		log.Println("error: failed to return web file: ", err)
+	}
+	defer inFile.Close()
+	fmt.Printf("File uploaded : %v\n", inFileHeader.Filename)
+	fmt.Printf("File size : %v\n", inFileHeader.Size)
+	fmt.Printf("File MIME header : %v\n", inFileHeader.Header)
+
+	// ------------------------- Creating main image ----------------------------------
+
+	mainOutFile, err := ioutil.TempFile("./", "tmp100-*.jpg")
+	if err != nil {
+		log.Println("error: creating TempFile: ", err)
+	}
+
+	if err := shrinkImage(inFile, mainOutFile, thumbnailSize); err != nil {
+		log.Println("error: shrink image failed: ", err)
+	}
+
+	mainOutFile.Close()
+	// ------------------------- Creating thumbnail ----------------------------------
+	thumbOutFile, err := ioutil.TempFile("./", "tmp400-*.jpg")
+	if err != nil {
+		log.Println("error: creating TempFile: ", err)
+	}
+
+	//Seek back to the beginning of the file, so we can read it once more.
+	_, err = inFile.Seek(0, 0)
+	if err != nil {
+		log.Println("error: Failed seek to the start of read file: ", err)
+	}
+
+	if err := shrinkImage(inFile, thumbOutFile, mainImageSize); err != nil {
+		log.Println("error: shrink image failed: ", err)
+	}
+
+	thumbOutFile.Close()
+}
+
+// -----------------------------------------------------------------------
+// -------------------------------- Main HTTP ----------------------------
+
+type data struct {
+	templ *template.Template
+}
+
+//newData will return a *data, which holds all the templates parsed.
+func newData() *data {
+	t, err := template.ParseFiles("./static/index.html", "./static/upload.html")
+	if err != nil {
+		log.Println("error: failed parsing template: ", err)
+	}
+
+	return &data{templ: t}
+}
+
+func (d *data) mainPage(w http.ResponseWriter, r *http.Request) {
+	err := d.templ.ExecuteTemplate(w, "mainHTML", nil)
+	if err != nil {
+		log.Println("error: executing template: ", err)
+	}
+}
+
+func main() {
+	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./static"))))
+	d := newData()
+
+	http.HandleFunc("/", d.mainPage)
+	http.HandleFunc("/upload", d.uploadImage)
+
+	fmt.Println("Web server started, listening at port ", serverListenPort)
+	err := http.ListenAndServe(serverListenPort, nil)
+	if err != nil {
+		log.Println("error: ListenAndServer failed: ", err)
+	}
+}
