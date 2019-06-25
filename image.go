632bed2bca8e036ff54f68f59586bcd2b0690c39
Bjørn Tore Svinningen <postmannen@gmail.com>
2019-06-25 21:04:32 +0200

    switching singin button

---
+++ image.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/jpeg"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+
+	"github.com/nfnt/resize"
+)
+
+const (
+	mainImageSize = 700
+	thumbnailSize = 200
+)
+
+//uploadImage will open a form for the user for uploading images.
+// 2 images will be produced in the function and saved to disk.
+// One main image, and one thumbnail.
+func (d *server) uploadImage(w http.ResponseWriter, r *http.Request) {
+	if err := d.templ.ExecuteTemplate(w, "upload", d); err != nil {
+		log.Println("error: failed executing template for upload: ", err)
+	}
+
+	var err error
+
+	//Takes max size of form to parse, so here we can limit the size of the image to upload.
+	err = r.ParseMultipartForm(32 << 20)
+	if err != nil {
+		log.Println("error: ParseMultipartForm: ", err)
+		//NB: NB: We have to return here if it fails, since the first show of the
+		// page no files are specified, and the page will panic on the code below
+		// if it continues and try to get the FormFile.
+		return
+	}
+
+	//Get a handler for the file found in the web form.
+	//Returns the first file for the provided key.
+	inFile, inFileHeader, err := r.FormFile("myFile")
+	if err != nil {
+		log.Println("error: failed to return web file: ", err)
+		return
+	}
+	defer inFile.Close()
+	fmt.Printf("File uploaded : %v\n", inFileHeader.Filename)
+	fmt.Printf("File size : %v\n", inFileHeader.Size)
+	fmt.Printf("File MIME header : %v\n", inFileHeader.Header)
+
+	// ------------------------- Creating main image ----------------------------------
+
+	mainOutFile, err := ioutil.TempFile("./", "tmp100-*.jpg")
+	if err != nil {
+		log.Println("error: creating TempFile: ", err)
+		return
+	}
+
+	if err := shrinkImage(inFile, mainOutFile, thumbnailSize); err != nil {
+		log.Println("error: shrink image failed: ", err)
+	}
+
+	mainOutFile.Close()
+	// ------------------------- Creating thumbnail ----------------------------------
+	thumbOutFile, err := ioutil.TempFile("./", "tmp400-*.jpg")
+	if err != nil {
+		log.Println("error: creating TempFile: ", err)
+	}
+
+	//Seek back to the beginning of the file, so we can read it once more.
+	_, err = inFile.Seek(0, 0)
+	if err != nil {
+		log.Println("error: Failed seek to the start of read file: ", err)
+	}
+
+	if err := shrinkImage(inFile, thumbOutFile, mainImageSize); err != nil {
+		log.Println("error: shrink image failed: ", err)
+	}
+
+	thumbOutFile.Close()
+}
+
+//shrinkImage will shrink an image to the width specified in size, and keep
+// the verical aspect ratio for that width.
+func shrinkImage(inReader io.Reader, outWriter io.Writer, size uint) error {
+	decIm, _, err := image.Decode(inReader)
+	if err != nil {
+		return err
+	}
+
+	rezIm := resize.Resize(size, 0, decIm, resize.Lanczos3)
+
+	err = jpeg.Encode(outWriter, rezIm, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -8,108 +8,17 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"html/template"
-	"image"
-	"image/jpeg"
-	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
 	"os"
 
+	"github.com/gorilla/sessions"
+
 	"github.com/mholt/certmagic"
-	"github.com/nfnt/resize"
 	"github.com/postmannen/authsession"
 )
-
-//shrinkImage will shrink an image to the width specified in size, and keep
-// the verical aspect ratio for that width.
-func shrinkImage(inReader io.Reader, outWriter io.Writer, size uint) error {
-	decIm, _, err := image.Decode(inReader)
-	if err != nil {
-		return err
-	}
-
-	rezIm := resize.Resize(size, 0, decIm, resize.Lanczos3)
-
-	err = jpeg.Encode(outWriter, rezIm, nil)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-const (
-	mainImageSize = 700
-	thumbnailSize = 200
-)
-
-//uploadImage will open a form for the user for uploading images.
-// 2 images will be produced in the function and saved to disk.
-// One main image, and one thumbnail.
-func (d *server) uploadImage(w http.ResponseWriter, r *http.Request) {
-	if err := d.templ.ExecuteTemplate(w, "upload", d); err != nil {
-		log.Println("error: failed executing template for upload: ", err)
-	}
-
-	var err error
-
-	//Takes max size of form to parse, so here we can limit the size of the image to upload.
-	err = r.ParseMultipartForm(32 << 20)
-	if err != nil {
-		log.Println("error: ParseMultipartForm: ", err)
-		//NB: NB: We have to return here if it fails, since the first show of the
-		// page no files are specified, and the page will panic on the code below
-		// if it continues and try to get the FormFile.
-		return
-	}
-
-	//Get a handler for the file found in the web form.
-	//Returns the first file for the provided key.
-	inFile, inFileHeader, err := r.FormFile("myFile")
-	if err != nil {
-		log.Println("error: failed to return web file: ", err)
-		return
-	}
-	defer inFile.Close()
-	fmt.Printf("File uploaded : %v\n", inFileHeader.Filename)
-	fmt.Printf("File size : %v\n", inFileHeader.Size)
-	fmt.Printf("File MIME header : %v\n", inFileHeader.Header)
-
-	// ------------------------- Creating main image ----------------------------------
-
-	mainOutFile, err := ioutil.TempFile("./", "tmp100-*.jpg")
-	if err != nil {
-		log.Println("error: creating TempFile: ", err)
-		return
-	}
-
-	if err := shrinkImage(inFile, mainOutFile, thumbnailSize); err != nil {
-		log.Println("error: shrink image failed: ", err)
-	}
-
-	mainOutFile.Close()
-	// ------------------------- Creating thumbnail ----------------------------------
-	thumbOutFile, err := ioutil.TempFile("./", "tmp400-*.jpg")
-	if err != nil {
-		log.Println("error: creating TempFile: ", err)
-	}
-
-	//Seek back to the beginning of the file, so we can read it once more.
-	_, err = inFile.Seek(0, 0)
-	if err != nil {
-		log.Println("error: Failed seek to the start of read file: ", err)
-	}
-
-	if err := shrinkImage(inFile, thumbOutFile, mainImageSize); err != nil {
-		log.Println("error: shrink image failed: ", err)
-	}
-
-	thumbOutFile.Close()
-}
 
 // -----------------------------------------------------------------------
 // -------------------------------- Main HTTP ----------------------------
@@ -117,11 +26,12 @@ func (d *server) uploadImage(w http.ResponseWriter, r *http.Request) {
 type server struct {
 	templ     *template.Template
 	UploadURL string //the whole url for upload, ex. http://fqdn/upload
+	store     *sessions.CookieStore
 }
 
 //newServer will return a *server, and will hold all the
 // server specific variables.
-func newServer(proto string, host string, port string) *server {
+func newServer(proto string, host string, port string, store *sessions.CookieStore) *server {
 	t, err := template.ParseFiles("./static/index.html", "./static/upload.html")
 	if err != nil {
 		log.Println("error: failed parsing template: ", err)
@@ -130,15 +40,31 @@ func newServer(proto string, host string, port string) *server {
 	return &server{
 		templ:     t,
 		UploadURL: proto + "://" + host + ":" + port + "/upload",
+		store:     store,
 	}
+}
+
+type TokenData struct {
+	Authenticated bool
+	ID            string
+	Fullame       string
+	Email         string
 }
 
 //mainPage is the main web page.
 func (d *server) mainPage(w http.ResponseWriter, r *http.Request) {
-	err := d.templ.ExecuteTemplate(w, "mainHTML", nil)
+	session, _ := d.store.Get(r, "cookie-name")
+
+	tplData := TokenData{
+		Email:         session.Values["email"].(string),
+		Authenticated: session.Values["authenticated"].(bool),
+	}
+
+	err := d.templ.ExecuteTemplate(w, "mainHTML", tplData)
 	if err != nil {
 		log.Println("error: executing template: ", err)
 	}
+
 }
 
 //handlers contains all the handlers used for this service.
@@ -156,16 +82,16 @@ func main() {
 	hostListen := flag.String("hostListen", "localhost", "The ip of the interface where the web server will listen. Typically 0.0.0.0 for an internet facing server")
 	flag.Parse()
 
-	//Greate a new server type that will hold all handlers, and web variable data.
-	d := newServer(*proto, *host, *port)
-
 	//Get secret values for authenticating to the google cloud app
 	// from environment variables. Then create a new 'auth', and start it.
 	cookieStoreKey := os.Getenv("cookiestorekey")
 	clientIDKey := os.Getenv("clientidkey")
 	clientSecret := os.Getenv("clientsecret")
-	a := authsession.NewAuth(*proto, *host, *port, cookieStoreKey, clientIDKey, clientSecret)
+	a, store := authsession.NewAuth(*proto, *host, *port, cookieStoreKey, clientIDKey, clientSecret)
 	a.Run()
+
+	//Greate a new server type that will hold all handlers, and web variable data.
+	d := newServer(*proto, *host, *port, store)
 
 	//Initialize the handlers for this program.
 	handlers(d, a)
